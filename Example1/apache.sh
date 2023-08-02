6ae7940e19a1e17e1399ed86ee759c053ab3bb96
ng.test88 <ng.test88@gmail.com>
2023-08-02 14:57:48 +0530

    changed apache config

--- Example1/apache.sh
+++ Example1/apache.sh
@@ -3,3 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start
+echo "<h1> This page Server has been deployed by Terraform Cloud </h1>" | sudo tee /var/www/html/index.html
