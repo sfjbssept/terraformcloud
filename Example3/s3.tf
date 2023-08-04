c80ea9305e06bce2cea19de6498990c020ec2d17
ng.test88 <ng.test88@gmail.com>
2023-08-04 17:31:51 +0530

    added new version

--- Example3/s3.tf
+++ Example3/s3.tf
@@ -1,6 +1,6 @@
 module "s3-module" {
   source  = "app.terraform.io/nishant_tcs_org/s3-module/cloud"
-  version = "1.0.0"
+  version = "3.0.0"
   name_bucket = var.s3_bucket_name
   tags_string = var.s3_bucket_tag
 }
